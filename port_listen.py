5e62b5f8d71352b12e1900b1dc6c61337955dbf7
Emir Zekeriya Tüccar <94643722+EmirTuccar@users.noreply.github.com>
2023-08-29 16:57:12 +0300

    Add files via upload

--- port_listen.py
+++ port_listen.py
@@ -1,67 +1,125 @@
 import socket
 import os
 import subprocess
+import threading
+import time
 
 def modify_firewall(action):
     command = ["sudo", "iptables", "-I", "INPUT", "-p", "tcp", "--dport", "12345", "-j", action]
     subprocess.run(command, check=True)
 
+password = 1
+
+def controller():
+    global password
+    if password == 1:
+        modify_firewall("ACCEPT")
+    elif password == 0:
+        modify_firewall("DROP")
 
 def start_listener(host, port):
-    # Create a socket object
+    print("-----------------------------------------------------------")
+    
     listener_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
     try:
-        # Bind the socket to a specific address and port
-        listener_socket.bind((host, port))
         
-        # Listen for incoming connections
+        listener_socket.bind((host, port))
         listener_socket.listen(1)
         print(f"Listening on {host}:{port}")
         
         while True:
-            # Accept a new connection
-            #os.system("sudo iptables -I INPUT -p tcp --dport 12345 -j ACCEPT")
-            modify_firewall("ACCEPT")
+            controller()
             client_socket, client_address = listener_socket.accept()
-            
-            if client_address[0] == "#Desired_ip_address":
 
-                
-                print(f"Accepted connection from {client_address}")
-                
-            
-                # Receive and process data from the client
-                data = client_socket.recv(1024).decode('utf-8')
-                print(f"Received data: {data}")
-                
-            
-                # Respond to the client
-                response ="\n|\    \ \ \ \ \ \ \      __\n|  \    \ \ \ \ \ \ \   | O~-_\n|   >----|-|-|-|-|-|-|--|  __/\n|  /    / / / / / / /   |__\ \n|/     / / / / / / / \n "
+            if client_address[0] == "192.168.56.101":
+                modify_firewall("ACCEPT")
 
-
-                
-
-                client_socket.sendall(response.encode('utf-8'))
-                print("response: " + response)
-            
-                # Close the client socket
-                client_socket.close()
-            else:
-                
-                
-                #modify_firewall("DROP")
-                listener_socket.close()
+            # Handle each client in a separate thread
+            client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+            client_thread.start()
             
     except Exception as e:
         print(f"Error: {e}")
-        os.system("sudo iptables -I INPUT -p tcp --dport 12345 -j DROP")
+        #modify_firewall("DROP")
 
     finally:
-        # Close the listener socket
         listener_socket.close()
+        #modify_firewall("DROP")
+
+def accepted(client_socket, client_address):
+    modify_firewall("ACCEPT")
+    print(f"Accepted connection from {client_address}")
+    
+    data = client_socket.recv(1024).decode('utf-8')
+    print(f"Received data: {data}")
+
+    response = "\n|\    \ \ \ \ \ \ \      __\n|  \    \ \ \ \ \ \ \   | O~-_\n|   >----|-|-|-|-|-|-|--|  __/\n|  /    / / / / / / /   |__\ \n|/     / / / / / / / \n "
+    client_socket.sendall(response.encode('utf-8'))
+    print("response: " + response)
+
+    global password
+
+    password = 1
+
+
+def denied(client_socket, client_address):
+    
+    response = "Access denied."
+            
+    print("response: " + response)
+    #modify_firewall("DROP")
+
+    os.system("sudo iptables -I INPUT -p tcp -s" + client_address[0] + "-j ACCEPT ")
+    os.system("sudo iptables -I OUTPUT -p tcp -d" + client_address[0] + "-j ACCEPT")
+
+    
+
+
+
+    #global password
+    #password = 0
+
+def handle_client(client_socket, client_address):
+    controller()
+    try:
+        if client_address[0] == "192.168.56.101":
+            accepted(client_socket,client_address)
+            """
+            modify_firewall("ACCEPT")
+            print(f"Accepted connection from {client_address}")
+
+            # Modify firewall to ACCEPT for authorized client
+            
+
+            # Receive and process data from the client
+            data = client_socket.recv(1024).decode('utf-8')
+            print(f"Received data: {data}")
+            modify_firewall("ACCEPT")
+            # Respond to the client
+            response = "\n|\    \ \ \ \ \ \ \      __\n|  \    \ \ \ \ \ \ \   | O~-_\n|   >----|-|-|-|-|-|-|--|  __/\n|  /    / / / / / / /   |__\ \n|/     / / / / / / / \n "
+            client_socket.sendall(response.encode('utf-8'))
+            print("response: " + response)"""
+        else:
+            denied(client_socket,client_address)
+            """
+            # Respond to the client
+            response = "Access denied."
+            
+            print("response: " + response)
+            modify_firewall("DROP")"""
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+    finally:
+        client_socket.close()
+        print("lolo")
 
 if __name__ == "__main__":
-    host = input("ip: ")  # Listen on all available interfaces
-    port = int(input("port: "))   # Choose a port number
+    #modify_firewall("ACCEPT")
+    #os.system("sudo iptables -I INPUT -p tcp --dport 12345 -j ACCEPT")
+    host = input("ip: ")
+    port = int(input("port: "))
+    modify_firewall("ACCEPT")  # Initialize the firewall rule
     start_listener(host, port)
